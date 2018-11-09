320711a63f5b4bac34a976276a58926eec421a94
Lolloo8 <44896130+Lolloo8@users.noreply.github.com>
2018-11-09 12:02:08 +0100

    Add files via upload

---
+++ Login/script.js
@@ -0,0 +1,6 @@
+function validate(){
+  var username = document.getElementById('nome');
+  var password = document.getElementById('password');
+
+}
+var data = document.getElementById("data").innerHTML = Date();
